a5278ce58c15416890cfaaf7f90383d67d5bf261
Sarah <sarahhale95@gmail.com>
2018-04-06 13:37:56 +0200

    filter

--- afi-lab-2/src/table.js
+++ afi-lab-2/src/table.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+
 import { Query } from "react-apollo"
 import gql from "graphql-tag";
 
@@ -10,10 +13,12 @@ const columns = [{
   text: 'ID'
 }, {
   dataField: 'name',
-  text: 'Name'
+  text: 'Name',
+  filter: textFilter()
 }, {
   dataField: 'town.name',
-  text: 'Town'
+  text: 'Town',
+  filter: textFilter()
 }];
 
 export default () =>
@@ -35,6 +40,6 @@ export default () =>
   {({ loading, error, data }) => {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
-      return <BootstrapTable keyField='name' data={ data.people } columns={ columns } />
+      return <BootstrapTable keyField='name' data={ data.people } columns={ columns } filter={ filterFactory()} />
   }}
 </Query>
