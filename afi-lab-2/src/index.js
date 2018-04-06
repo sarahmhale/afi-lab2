0b5eff6eee0fa796ab5253284e078699e66086a0
Sarah <sarahhale95@gmail.com>
2018-04-06 13:31:46 +0200

    showing data

--- afi-lab-2/src/index.js
+++ afi-lab-2/src/index.js
@@ -7,7 +7,7 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 import Table from './table'
 const client = new ApolloClient({
-  uri: "https://127.0.0.1:4000/graphql"
+  uri: "http://127.0.0.1:4000/graphql"
 });
 
 const App = () => (
@@ -16,7 +16,7 @@ const App = () => (
   <ApolloProvider client={client}>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
     <div>
-      <h2>My first Apollo app 🚀</h2>
+      <h2>My first Apollo app </h2>
       <Table/>
     </div>
   </ApolloProvider>
--- afi-lab-2/src/table.js
+++ afi-lab-2/src/table.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import { Query } from "react-apollo"
+import gql from "graphql-tag";
 
-const row = [{
-  id: 'A001',
-  address: {
-    postal: '1234-12335',
-    city: 'Chicago'
-  }
-}]
+
 const columns = [{
-  dataField: 'id',
+  dataField: 'personID',
   text: 'ID'
 }, {
-  dataField: 'address.city',
+  dataField: 'name',
   text: 'Name'
 }, {
-  dataField: 'price',
+  dataField: 'town.name',
   text: 'Town'
 }];
 
 export default () =>
-<BootstrapTable keyField='name' data={ row } columns={ columns } />
+
+<Query
+  query={gql`
+        {
+          people{
+            personID
+            name
+            town {
+              id
+              name
+            }
+          }
+        }
+      `}
+>
+  {({ loading, error, data }) => {
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+      return <BootstrapTable keyField='name' data={ data.people } columns={ columns } />
+  }}
+</Query>
